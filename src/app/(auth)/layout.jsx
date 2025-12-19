eeab1ad0459188bdbe7e2d2d9d6da6bc966c9be3
Pandega <pandegarn@gmail.com>
2025-12-19 09:52:20 +0700

    fix 2

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { auth } from "../libs/auth";
+import { redirect } from "next/navigation";
 
-const layout = ({ children }) => {
+const layout = async ({ children }) => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/signin");
+  }
   return <div>{children}</div>;
 };
 
